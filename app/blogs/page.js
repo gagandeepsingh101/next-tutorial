e25fc1fe833bef918fdb6f927b8f2239859ac62a
gagandeepsingh101 <gagandeepsingh128548@gmail.com>
2023-12-05 21:04:28 +0530

    create blog and ticket page

---
+++ app/blogs/page.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import React from 'react'
+const page = () => {
+  return (
+    <div>
+      <h1>Blog Page</h1>
+      <Link href="/">Go Back Home</Link>
+    </div>
+  )
+}
+
+export default page
---
+++ app/ticket/page.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import React from 'react'
+const ticketList = () => {
+  return (
+    <div>
+      <h1>Ticket List</h1>
+      <Link href="/">Go Back Home</Link>
+    </div>
+  )
+}
+
+export default ticketList
